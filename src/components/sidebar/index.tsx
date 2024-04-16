d31d5e2eb5a917946ffa26643eef2b3ba560498f
Jyotsna <jyotsnaicloud@gmail.com>
2024-04-16 23:25:37 +0530

    setting up dark, light and system mode

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -13,7 +13,7 @@ import { menuOptions } from '@/lib/constant'
 import clsx from 'clsx'
 import { Separator } from '@/components/ui/separator'
 import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
-// import { ModeToggle } from '../global/mode-toggle'
+import { ModeToggle } from '../global/mode-toggle'
 
 type Props = {}
 
@@ -93,7 +93,7 @@ const MenuOptions = (props: Props) => {
         </div>
       </div>
       <div className="flex items-center justify-center flex-col gap-8">
-        {/* <ModeToggle /> */}
+        <ModeToggle />
       </div>
     </nav>
   )
